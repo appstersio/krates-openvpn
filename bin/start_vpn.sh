e991b2ea0eadc5fdbe58d8e3c34dc58fd3dd90b1
Jussi Nummelin <jussi.nummelin@digia.com>
2015-02-02 13:11:24 +0200

    Added fully automatic setup of both server side and single client config

--- bin/start_vpn.sh
+++ bin/start_vpn.sh
@@ -13,5 +13,16 @@ echo "\nWeave network active, proceeding with VPN startup"
 # Setup NAT for ethwe
 iptables -t nat -A POSTROUTING -o ethwe -j MASQUERADE
 
+OVPN_CA_PASSPHRASE=openssl rand -base64 32
+
+# Generate configs
+ovpn_genconfig -u $OVPN_SERVER_URL
+
+# Initialize PKI stuff
+ovpn_initpki nopass
+
+# Create client certs and config
+easyrsa build-client-full QTCS_VPN_CLIENT nopass
+
 # Start VPN normally
 ovpn_run
