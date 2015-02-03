9a9f8e3ea538bf04852557d40eeb84a48ac4de98
Jussi Nummelin <jussi.nummelin@digia.com>
2015-02-03 09:40:47 +0200

    Added sudo invocations to all commands as we've changed to ubuntu base...

--- bin/start_vpn.sh
+++ bin/start_vpn.sh
@@ -11,18 +11,18 @@ while ! [ -n "$(ip addr show | grep ethwe:)" ]; do
 done
 echo "\nWeave network active, proceeding with VPN startup"
 # Setup NAT for ethwe
-iptables -t nat -A POSTROUTING -o ethwe -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -o ethwe -j MASQUERADE
 
 OVPN_CA_PASSPHRASE=openssl rand -base64 32
 
 # Generate configs
-ovpn_genconfig -u $OVPN_SERVER_URL
+sudo ovpn_genconfig -u $OVPN_SERVER_URL
 
 # Initialize PKI stuff
-ovpn_initpki nopass
+sudo ovpn_initpki nopass
 
 # Create client certs and config
-easyrsa build-client-full QTCS_VPN_CLIENT nopass
+sudo easyrsa build-client-full QTCS_VPN_CLIENT nopass
 
 # Start VPN normally
-ovpn_run
+sudo ovpn_run
