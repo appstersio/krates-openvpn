17cef06318d00b3da6735c692cb28b37adb41430
Jussi Nummelin <jussi.nummelin@digia.com>
2015-02-05 13:36:33 +0200

    Allowed restarting of container

--- bin/start_vpn.sh
+++ bin/start_vpn.sh
@@ -15,16 +15,23 @@ iptables -t nat -A POSTROUTING -o ethwe -j MASQUERADE
 
 cp /usr/local/share/easy-rsa/easyrsa3/openssl-1.0.cnf /etc/openvpn
 
-OVPN_CA_PASSPHRASE=openssl rand -base64 32
-
-# Generate configs
+# Generate configs, can be pretty safely overwritten
 ovpn_genconfig -u $OVPN_SERVER_URL
 
-# Initialize PKI stuff
-ovpn_initpki nopass
+# Initialize PKI stuff, need to guard these as the EasyRSA 
+# would override the existing config
+if [ ! -f "$EASYRSA_PKI/private/ca.key" ]; then
+    ovpn_initpki nopass
+else
+    echo "PKI already initialized, using old configuration"
+fi
 
 # Create client certs and config
-easyrsa build-client-full QTCS_VPN_CLIENT nopass
+if [ ! -f "$EASYRSA_PKI/private/QTCS_VPN_CLIENT.key" ]; then
+    easyrsa build-client-full QTCS_VPN_CLIENT nopass
+else
+    echo "Client certificate already initialized, using old configuration"
+fi
 
 # Start VPN normally
 ovpn_run
