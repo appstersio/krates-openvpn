4619bf5a85be051d8cf3c1ec31ecc2c7c56f4b5e
Jari Kolehmainen <jari.kolehmainen@digia.com>
2015-04-27 19:34:06 +0300

    remove weave wait

--- bin/start_vpn.sh
+++ bin/start_vpn.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Wrapper script to enable some waiting for Weave network to become active
-# If VPN is activated before Weave, Weave network doesn't work :(
-
-echo "Waiting for ethwe to become active"
-
-while ! [ -n "$(ip addr show | grep ethwe:)" ]; do
-    sleep 1
-    printf "."
-done
-echo "\nWeave network active, proceeding with VPN startup"
-# Setup NAT for ethwe
-iptables -t nat -A POSTROUTING -o ethwe -j MASQUERADE
-
 cp /usr/local/share/easy-rsa/easyrsa3/openssl-1.0.cnf /etc/openvpn
 
 # Generate configs, can be pretty safely overwritten
